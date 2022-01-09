1633e2306243ea5c85811b58e2b91eeccf0e6f46
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 13:43:34 +0100

    177 gotowe pliki css

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
-function HomePage() {}
+// 1) Hero section (zwyczajowa nazwa) - welcome section when we presents products our ourself
+// 2) Featured Posts
+import { Fragment } from "react";
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturesPosts/>
+    </Fragment>
+  );
+}
 
 export default HomePage;
