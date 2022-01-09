349ba8c8422274eb23626dba6ef32bdd35a39302
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 21:42:27 +0100

    192 NIE DZIALA <ReactMarkdown renderers

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,25 +1,62 @@
 //markdown krótszy od html ale może zostać w niego zmieniony(HTML) albo w JSX z jakąs dodatkową bilbioteką
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import PostHeader from "./post-header";
-import classes from './post-content.module.css';
+import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs1",
-  title: "Getting started with Nextjs",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# Jakis długi kontent w markdwon", // # zamieni na tytuł teraz stringa w htmla trzeba zamienić
-};
+function PostConentent(props) {
+  const { post } = props;
 
-function PostConentent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+
+  //mówic markdownowi jak ma nadpisać defaultowe  ustawienia - sprawa ładnego wyświetlania obrazka
+  const customRenderers = {
+    // img(image) {
+    //   // metoda z React MArkdown ma atrybut image i dnormalnie zwraca <img/> NIE korzystając z nextjs Image
+    //   //image argument alttext to ten z markdowna w []
+    //   //image.src to nazwa pliku z markdowna (nextjs-file-based-routing.png)
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       // alt={image.alt}
+    //       alt={image.src}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      //obrazek jest w paragrafie - to robi nextjs
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`images/posts/${post.slug}/${image.properties.src}`}
+              // alt={image.alt}
+              alt={`images/posts/${post.slug}/${image.properties.src}`}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      // jak nie ma to zwracam zwykły paragraf
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath} />
 
       {/* z markdowna do (html) jsx */}
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,12 +1,13 @@
 import PostConentent from "../../components/posts/post-detail/post-content";
-import { getPostData } from "../../lib/posts-util";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
 // slug to unikalne id ale human readble np /posts/jakis-tytul-unikalny-posta
-function PostDetailPage() {
-  return <PostConentent />;
+function PostDetailPage(props) {
+  return <PostConentent post={props.post}/>;
 }
 
-export function getStaticProps(context) {//nieasynchronicza bo synchornicze czytam pliki
+export function getStaticProps(context) {
+  //nieasynchronicza bo synchornicze czytam pliki
   const { params } = context; // contex ma wartosc id sluga
   const { slug } = params;
 
@@ -21,17 +22,20 @@ export function getStaticProps(context) {//nieasynchronicza bo synchornicze czyt
   };
 }
 
-export function getStaticPaths(){//nieasynchronicza bo synchornicze czytam pliki
-    return {
-        //to teraz dane będą pobieran ai tworzone na żadanie
-        paths: [],
-        // fallback: true //trzeba by jeszcze jakiś fallback contnet dodać, gdy sie nie dodały jeszcze dane i niewyrenderował JSX
-        
-        fallback: 'blocking', // teraz sie nie wyświetli dopóki nie wyrenderuje danych, dobre gdy ma się blog z ysiacem postów a cześć z nich jest żadko albo wcale odwiedzana
-        // a jednoczesnie nie chcesz prerenderować danych la wszystkich tych postów
-        //albo dobre gdy się prerender tylko część postów np.: tych najpopularniejszych
-    }
+export function getStaticPaths() {
+  //nieasynchronicza bo synchornicze czytam pliki
+  const postFilenames = getPostsFiles();
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/,'')); // pozbycie się rozszerzenia z pliku
 
+  return {
+    //to teraz dane będą pobieran ai tworzone na żadanie
+    paths: slugs.map((slug) => ({ params: { slug: slug } })), //paths WYMAGA listy OBIEKTOW z params kluczem
+    // fallback: true //trzeba by jeszcze jakiś fallback contnet dodać, gdy sie nie dodały jeszcze dane i niewyrenderował JSX
+
+    fallback: "blocking", // teraz sie nie wyświetli dopóki nie wyrenderuje danych, dobre gdy ma się blog z ysiacem postów a cześć z nich jest żadko albo wcale odwiedzana
+    // a jednoczesnie nie chcesz prerenderować danych la wszystkich tych postów
+    //albo dobre gdy się prerender tylko część postów np.: tych najpopularniejszych
+  };
 }
 
 export default PostDetailPage;
