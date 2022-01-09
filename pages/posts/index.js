f672d0d51cef4e518061f691b4e8baa7cbcefd43
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 18:12:31 +0100

    184 AllPostsPage i AllPosts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POST = [
+    {
+      slug: "getting-started-with-nextjs1",
+      title: "Getting started with Nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "Nextjs is a React framework for production",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs2",
+      title: "Getting started with Nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "Nextjs is a React framework for production",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs3",
+      title: "Getting started with Nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "Nextjs is a React framework for production",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs4",
+      title: "Getting started with Nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "Nextjs is a React framework for production",
+      date: "2022-02-10",
+    },
+  ];
+
+function AllPostsPage() {
+    return <AllPosts posts={DUMMY_POST}/>
+}
 
 export default AllPostsPage;
