922ad3e641c0d3298f2fc09510c6da2422b70740
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 23:05:01 +0100

    197 zapis emaili w Mongodb - polaczenie z baza

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,9 @@
 //  /api/contact w url/endpoin API
-function handler(req, res) {
+//fetcha nie ma, bo nie pokazuje sie emaili ludzi z newslettera
+
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     //reszte metod ignoruję
     const { email, name, message } = req.body; // next.js od razu parsuje ładnie dane
@@ -13,19 +17,43 @@ function handler(req, res) {
       !message ||
       message.trim() === ""
     ) {
-        res.status(422).json({message: 'Invalid input.'})
-        return;
+      res.status(422).json({ message: "Invalid input." });
+      return;
     }
 
     // Stroe in a database
     const newMessage = {
-        email,
-        name,
-        message
+      email,
+      name,
+      message,
+    };
+
+    // połaczenie z bazą powa lidacji danych
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "string do bazy"
+      );
+    } catch (error) {
+      resizeTo.status(500).json({ message: "Could not connect to database!" });
+      return;
     }
 
+    const db = client.db();
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      //dodanie dodatkowego pola id albo _id
+      newMessage._id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storign message failed!" });
+      resturn;
+    }
+    // zamknać połączenie z baza w przypadku sukcesu też
+    client.close();
+
     console.log(newMessage);
-    res.status(201).json({message: 'Succesfuly stroed message!'})
+    res.status(201).json({ message: "Succesfuly stroed message!" , message: newMessage});
   }
 }
 
