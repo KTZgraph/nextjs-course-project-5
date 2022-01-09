337e0a1a29fd436c18ad4eb99991f53e02a91c98
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 13:55:38 +0100

    178 hero components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 // 1) Hero section (zwyczajowa nazwa) - welcome section when we presents products our ourself
 // 2) Featured Posts
 import { Fragment } from "react";
+import Hero from '../components/home-page/hero';
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturesPosts/>
+      {/* <FeaturesPosts/> */}
     </Fragment>
   );
 }
