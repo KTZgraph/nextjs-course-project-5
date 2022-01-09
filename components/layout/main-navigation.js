fcf7ba7ed3c448f111245362c1c7c4af96d4fc89
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 14:12:29 +0100

    179 prosty layout i nawigacja

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,29 @@
+// głowna nawigacja - layout jest podzielony na pliku
+// tu powinno też być logo dla bloga
+import Link from "next/link";
+
+
+function MainNavigation() {
+  return (
+    <header>
+      <Link href="/">
+        {/* jak w Linku dziecko nie jeset zwykły tekstem to nie generuje tagu a - trzeba samemu dodac <a> */}
+        <a> {/* nie trzeba do a dodawać teraz href, fajnie bo wszsytko można w linka przekształcić*/}
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
