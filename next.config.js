4190883ec433ae366b94ddf32b12ea674a10bcf5
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-10 22:33:13 +0100

    209 examle  next.config.js with dummy credentials

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    //gdy jesteśmy w developerskim serwerze
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: "myuser",
+        mongodb_password: "myuserpasssowrd",
+        mongodb_clustername: "culstermonodb",
+        mongodb_database: "db-dev",
+      },
+    };
+  }
+
+  // gdy jestesmy w produckji
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: "myuserproduction",
+      mongodb_password: "myuserproductionpasssowrd",
+      mongodb_clustername: "culstermonodb",
+      mongodb_database: "db",
+    },
+  };
+};
