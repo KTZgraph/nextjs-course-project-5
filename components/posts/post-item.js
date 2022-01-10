dfbb4e83b203a86601a20201f936ecc2313080f9
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-10 20:10:58 +0100

    207 w nextjs od razu strony są lazy loading

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,21 +1,21 @@
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
-import classes from './post-item.module.css';
+import classes from "./post-item.module.css";
 
 function PostItem(props) {
   // elemnt listy <li> bo jestesstesmy w komponencie PostGrid który jest unordered list
   // całąść powinna być klikalna
-  const {title, image, excerpt, date, slug} = props.post;
+  const { title, image, excerpt, date, slug } = props.post;
 
-  const formattedDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long', //1 =? Janu
-    year: 'numeric'
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long", //1 =? Janu
+    year: "numeric",
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
-  const linkPath = `/posts/${slug}`
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
@@ -24,7 +24,13 @@ function PostItem(props) {
           {/* trzeba dodać <a> bo skompilowaka cała struktura ma być linkiem */}
           <div className={classes.image}>
             {/* layout żeby siatka postów ładnie się zachodziła layout='responsive' będą rosły/malały razem z kontenerem */}
-            <Image src={imagePath} alt={title} width={300} height={200} layout='responsive'/>
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout="responsive"
+            />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>
